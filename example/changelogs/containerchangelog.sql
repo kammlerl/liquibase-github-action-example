573d6e0a07ad915e23d42a2e24db25f7b223586e
Continuous Integration <kammlerl@users.noreply.github.com>
2023-01-06 13:33:44 +0000

    Continuous Integration Changelog

--- example/changelogs/containerchangelog.sql
+++ example/changelogs/containerchangelog.sql
@@ -1,1 +1,6 @@
 
+-- liquibase formatted sql
+
+-- changeset liquibase:1673012024524-1
+CREATE TABLE "test1" ("t1_id" INTEGER NOT NULL, "name" VARCHAR(255) NOT NULL, "test1" INTEGER, CONSTRAINT "Test1_pk" PRIMARY KEY ("t1_id"));
+
